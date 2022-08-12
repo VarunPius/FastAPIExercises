973b67d25e81f3f6aa2f4c61e154df832ec57923
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2022-08-12 15:22:24 -0700

    RESTful API project

---
+++ REST_FastAPI/models.py
@@ -0,0 +1,35 @@
+from typing import Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    student = "student"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    middle_name: Optional[str] = None
+    last_name: str
+    gender: Gender
+    roles: List[Role]
+    wallet: Optional[int] = None
+    active: Optional[bool] = True 
+
+
+class UserUpdateRequest(BaseModel):
+    first_name: Optional[str]
+    middle_name: Optional[str] = None
+    last_name: Optional[str]
+    roles: Optional[List[Role]]
+    
+
