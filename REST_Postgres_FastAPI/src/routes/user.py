674fc2c44c896872134fe30530a6d62265e1b601
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-07-06 08:13:12 -0700

    get user with id route

--- REST_Postgres_FastAPI/src/routes/user.py
+++ REST_Postgres_FastAPI/src/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 #from src.config.db import conn
 from src.models.index import UsersModel
 from src.schemas.index import User
@@ -10,20 +10,54 @@ from src.config.db import SessionLocal
 user = APIRouter()
 db = SessionLocal()
 
-'''
-@user.get("/", response_model=List[User], status_code=200)
-async def read_data():
-    return conn.execute(users.select()).fetchall()      # `users` from models
-'''
 
-@user.get('/',response_model=List[User],status_code=200)
-def get_all_items():
+@user.get('/users',response_model=List[User],status_code=200)
+async def read_users():
     items = db.query(UsersModel).all()
 
     return items
 
 
+@user.get("/user/{id}", response_model=User, status_code=status.HTTP_200_OK)
+async def read_user(id: int):
+    item = db.query(UsersModel).filter(UsersModel.id == id).first()
+    return item
+
+
 '''
+@user.post("/")
+async def write_data(user: User):           # `User` from schema
+    conn.execute(users.insert().values(
+        name =  user.name,
+        email = user.email,
+        password = user.password
+    )
+    )
+    return conn.execute(users.select()).fetchall()
+
+@user.put("/{id}")
+async def update_data(id: int, user: User):           # `User` from schema
+    conn.execute(users.update().values(
+        name =  user.name,
+        email = user.email,
+        password = user.password
+    ).where(users.c.id == id)
+    )
+    return conn.execute(users.select()).fetchall()
+
+@user.delete("/")
+async def delete_data():
+    conn.execute(users.delete().where(users.c.id == id))
+    return conn.execute(users.select()).fetchall()      # `users` from models
+'''
+
+'''
+Old code:
+
+@user.get("/")
+async def read_data():
+    return conn.execute(users.select()).fetchall()      # `users` from models
+
 @user.get("/{id}")
 async def read_data(id: int):
     return conn.execute(users.select().where(users.c.id == id)).fetchall()
