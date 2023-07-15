a324fdbf551019bdef8d4186b65209cb34cd12ab
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-07-15 10:15:19 -0700

    db config

--- REST_Postgres_FastAPI/src/config/db.py
+++ REST_Postgres_FastAPI/src/config/db.py
@@ -1,3 +1,31 @@
 from sqlalchemy import create_engine
-from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+
+# engine=create_engine("postgresql://<user>:<pwd>@<host>/<db>",
+conn_url = 'postgresql+psycopg2://vpiusr:Seatt!3@postgres_db/postgres'
+engine = create_engine(conn_url, echo=True)
+
+Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine)
+
+
+'''
+Old Code:
+from sqlalchemy import  MetaData
+from sqlalchemy.engine import URL
+url = URL.create(
+    drivername="vpiusr",
+    username="coderpad",
+    host="/tmp/postgresql/socket",
+    database="postgres"
+)
+engine = create_engine(url)
+# or 
+engine = create_engine(conn_url)
+
+conn = engine.connect()
+
+meta = MetaData()       # meta is used in models if creating object using `Table`
+'''
