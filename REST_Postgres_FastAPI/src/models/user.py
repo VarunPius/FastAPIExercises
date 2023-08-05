a21ace0e85c0449024151b8d19cdb071231b8943
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-08-05 11:11:48 -0700

    User model

--- REST_Postgres_FastAPI/src/models/user.py
+++ REST_Postgres_FastAPI/src/models/user.py
@@ -16,7 +16,21 @@ from sqlalchemy.sql.sqltypes import Integer, String, Text
 
 # Internal imports
 from src.config.db import Base
-#from src.config.db import meta
+
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Models 
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+class UsersModel(Base):
+    __tablename__='users'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(255), nullable = False)
+    email = Column(Text, unique = True)
+    address = Column(Text)
+
+    def __repr__(self):
+        return f"<User name = {self.name} |-> email = {self.email}>"
 
 
 
@@ -26,6 +40,12 @@ from src.config.db import Base
 
 '''
 Old code:
+from sqlalchemy.sql.expression import null
+from database import Base
+from src.config.db import meta
+from sqlalchemy import String,Boolean,Integer,Column,Text
+
+
 users = Table(
     'users', meta,
     Column('id', Integer, primary_key = True),
@@ -33,34 +53,5 @@ users = Table(
     Column('email', String(255)),
     Column('password', String(255))
 )
-'''
 
-class UsersModel(Base):
-    __tablename__='users'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(255),nullable = False,unique = True)
-    email = Column(Text)
-    address = Column(Text)
-
-    def __repr__(self):
-        return f"<Item name = {self.name} |->email = {self.email}>"
-
-
-'''
-from sqlalchemy.sql.expression import null
-from database import Base
-from sqlalchemy import String,Boolean,Integer,Column,Text
-
-
-class Item(Base):
-    __tablename__='items'
-    id=Column(Integer,primary_key=True)
-    name=Column(String(255),nullable=False,unique=True)
-    description=Column(Text)
-    price=Column(Integer,nullable=False)
-    on_offer=Column(Boolean,default=False)
-
-
-    def __repr__(self):
-        return f"<Item name={self.name} price={self.price}>"
 '''
