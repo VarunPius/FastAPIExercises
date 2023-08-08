8eeebb2e9cca1cd8715d57769e4d744b5664c823
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-08-08 14:38:17 -0700

    user route rearranging file

--- REST_Postgres_FastAPI/src/routes/user.py
+++ REST_Postgres_FastAPI/src/routes/user.py
@@ -42,21 +42,39 @@ async def create_user(user: User):
                         # if you need "Success" or "new user added" use `str`
 
 
-'''
-@user.put("/{id}")
-async def update_data(id: int, user: User):           # `User` from schema
-    conn.execute(users.update().values(
-        name =  user.name,
-        email = user.email,
-        password = user.password
-    ).where(users.c.id == id)
-    )
-    return conn.execute(users.select()).fetchall()
 
-@user.delete("/")
-async def delete_data():
-    conn.execute(users.delete().where(users.c.id == id))
-    return conn.execute(users.select()).fetchall()      # `users` from models
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Appendix
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+'''
+Example Code:
+
+# idea
+@app.put('/item/{item_id}',response_model=Item,status_code=status.HTTP_200_OK)
+def update_an_item(item_id:int,item:Item):
+    item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
+    item_to_update.name=item.name
+    item_to_update.price=item.price
+    item_to_update.description=item.description
+    item_to_update.on_offer=item.on_offer
+
+    db.commit()
+
+    return item_to_update
+
+@app.delete('/item/{item_id}')
+def delete_item(item_id:int):
+    item_to_delete=db.query(models.Item).filter(models.Item.id==item_id).first()
+
+    if item_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Resource Not Found")
+    
+    db.delete(item_to_delete)
+    db.commit()
+
+    return item_to_delete
+
 '''
 
 '''
@@ -95,28 +113,4 @@ async def delete_data():
     conn.execute(users.delete().where(users.c.id == id))
     return conn.execute(users.select()).fetchall()      # `users` from models
 
-# idea
-@app.put('/item/{item_id}',response_model=Item,status_code=status.HTTP_200_OK)
-def update_an_item(item_id:int,item:Item):
-    item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
-    item_to_update.name=item.name
-    item_to_update.price=item.price
-    item_to_update.description=item.description
-    item_to_update.on_offer=item.on_offer
-
-    db.commit()
-
-    return item_to_update
-
-@app.delete('/item/{item_id}')
-def delete_item(item_id:int):
-    item_to_delete=db.query(models.Item).filter(models.Item.id==item_id).first()
-
-    if item_to_delete is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Resource Not Found")
-    
-    db.delete(item_to_delete)
-    db.commit()
-
-    return item_to_delete
 '''
